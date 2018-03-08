4d68a165de2c2ac7620cb23a554228d7fee38f8a
Renzo Scuderi <renzo.scuderi@gmail.com>
2018-03-08 11:53:45 +0100

    recherche des nearbyPlacs + marker et infoBulle

--- src/app/gmap/gmap.component.ts
+++ src/app/gmap/gmap.component.ts
@@ -9,7 +9,7 @@ import { } from '@types/googlemaps';
   styleUrls: ['./gmap.component.css']
 })
 export class GmapComponent implements OnInit {
-  
+
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
 
@@ -19,10 +19,11 @@ export class GmapComponent implements OnInit {
   constructor(
 
     private ngZone: NgZone
-  ) {}
+  ) { }
 
-  latitude:number;
-  longitude:number;
+  latitude: number;
+  longitude: number;
+  markers = [];
 
   ngOnInit() {
     var mapProp = {
@@ -32,42 +33,94 @@ export class GmapComponent implements OnInit {
     };
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
 
-      google.maps.event.addListener(this.map, 'click', function(event) {
+    google.maps.event.addListener(this.map, 'click', function (event) {
+      // Map clic
+      console.log("marker added")
+    });
+
+
+    let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+      types: ["address"]
+    });
+    autocomplete.addListener("place_changed", () => {
+      this.ngZone.run(() => {
+        //get the place result
+        let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+        //verify result
+        if (place.geometry === undefined || place.geometry === null) {
+          return;
+        }
+
+        //set latitude, longitude and zoom
+        this.latitude = place.geometry.location.lat();
+        this.longitude = place.geometry.location.lng();
+
+        for (var i = 0; i < this.markers.length; ++i) {
+          this.markers[i].setMap(null);
+          this.markers.splice(i, 1);
+        }
+
         var marker = new google.maps.Marker({
-            position: event.latLng, 
-            map: this.map,
-            title : "home"
+          position: new google.maps.LatLng(this.latitude, this.longitude),
+          map: this.map,
+          title: "home"
         });
-        console.log("marker added")
-     });
 
+        var request = {
+          location: new google.maps.LatLng(this.latitude, this.longitude),
+          radius: 500,
+          query: 'restaurant'
+        };
 
-        let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-          types: ["address"]
-        });
-        autocomplete.addListener("place_changed", () => {
-          this.ngZone.run(() => {
-            //get the place result
-            let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-    
-            //verify result
-            if (place.geometry === undefined || place.geometry === null) {
-              return;
+        var service = new google.maps.places.PlacesService(this.map);
+        service.textSearch(request, callback.bind(this));
+
+        function callback(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              createMarker(results[i], this.map, this.markers);
             }
-            
-            //set latitude, longitude and zoom
-            this.latitude = place.geometry.location.lat();
-            this.longitude = place.geometry.location.lng();
+          }
+        }
+
+        function createMarker(place, map, markers) {
+          var infoWindow = new google.maps.InfoWindow();
+          var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location,
+            title: "Restaurant",
+            icon: "/assets/images/restaurant.png",
           });
-        });
+          marker.addListener('click', function () {
+            var request = { placeId: place.place_id };
 
-    }
+            service.getDetails(request, function (result, status) {
+              if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                console.error(status);
+                return;
+              }
+              infoWindow.setContent(result.name);
+              infoWindow.open(map, marker);
+            });
+          });
+          markers.push(marker)
+        }
 
-  setMapType(mapTypeId: string) {
-    this.map.setMapTypeId(mapTypeId)    
+        this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
+
+        this.markers.push(marker);
+      });
+    });
   }
 
-  setCenter(e:any){
+
+
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)
+  }
+
+  setCenter(e: any) {
     e.preventDefault();
     this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
   }
